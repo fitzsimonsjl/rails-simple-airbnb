469df9d0002c2acc7d907936eb4bc2c8acc294f9
Jack Fitzsimons <fitzsimonsjal@gmail.com>
2019-11-06 18:49:02 +0100

    styling sort of done. Needs wiring

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Flat.destroy_all
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Wonderful Flat Copenhagen',
+  address: 'Tranevej 10, 2. tv, 2200. Nørrebro, Copenhagen',
+  description: 'A lovely spacious flat tastefully decorated for maximum hygge. Double bed, and a decently sized kitchen',
+  price_per_night: 65,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Airy and spacious Flat Barcelona',
+  address: 'El Prat de Llobregat, Barcelona, Spain. 08820',
+  description: 'A good sized flat with all necessary amenities. Two double rooms and bathroom',
+  price_per_night: 35,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Cosy and minimalist Flat Reykjavík',
+  address: 'Aðalstræti 6, 101 Reykjavík, Iceland',
+  description: 'A cosy flat for two beautifully decorated. One large bed, and small kitchen. Open plan design.',
+  price_per_night: 80,
+  number_of_guests: 2
+)
+Flat.create!(
+  name: 'Retro and chic Flat Norway',
+  address: 'Sjøgata 7, 9259 Tromsø, Norway',
+  description: 'An open plan flat with chic and retro decor for a relaxed vibe. One double bed, one single.',
+  price_per_night: 80,
+  number_of_guests: 3
+)
